0f887636ea89aa7b2c80cac1b32a450293ac89d4
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2023-12-21 09:14:37 +0700

    updateupdate

---
+++ Javascript Lanjutan/ObjectJava.js
@@ -0,0 +1,56 @@
+//Object itu isinya properti dan method
+//Properti isinya nilai kalo method isinya function
+//di dalam object juga pake keyword this
+
+//Cara untuk membuat Object pada Javascript
+
+// 1.Object Literal
+// disini saya membuat object mahasiswa pake literal
+let mahasiswa1 = {
+    nama: 'Mohamad Iqbal Zaelani',
+    energy: 10,
+    makan: function(porsi){
+        this.energy = this.energy + porsi
+        console.log('Selamat makan $(this.nama), selamat makan')
+    }
+}
+let mahasiswa2 = {
+    nama: ' Zaelani Iqbal Mohamad',
+    energy: 10,
+    makan: function(porsi){
+        this.energy = this.energy + porsi
+        console.log('Selamat makan $(this.nama), selamat makan')
+    }
+}
+//Object Literal pakai {} yang isinya properti
+//method, method adalah fuction di dalam object
+//method disini makan
+//menggunakan literal kita bisa langsung ambil properti atau variable di dalam console.log dengan ${} tanpa perlu keluar string
+//kalo menggunakan object literal dan kita mau menambahkan mahasiwa kedua maka kita harus membuat lagi object nya
+
+
+
+// 2.Function Declaration
+//nah keuntungannya gaperlu buat object duplikat
+function Mahasiswa(nama, energy){
+    let mahasiswa = {}
+    mahasiswa.nama = nama
+    mahasiswa.energy = energy
+
+    mahasiswa.makan = function(porsi){
+        this.energy += energy
+        console.log('Selamat makan $(this.nama), selamat makan')
+    }
+    return Mahasiswa
+}
+
+let iqbal = Mahasiswa('Mohamad', 10)
+//penggunaan declaration itu object nya kosong tidak langsung di isi
+//perbedaanya juga yang tampil fuction bukan object
+
+//perbedaanya kalo mau bikin object nya kita hasus intasiasi
+//jadi buat variable untuk object lalu panggil function dan isi namanya tambahkan return object nya
+
+
+// 3.Constructor function
+// 4.Object.create
