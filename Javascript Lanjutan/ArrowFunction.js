ebe08dade9e283277a6fa252cc38d6e71500a517
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-03 10:08:09 +0700

    ArrowFunction

--- Javascript Lanjutan/ArrowFunction.js
+++ Javascript Lanjutan/ArrowFunction.js
@@ -25,3 +25,39 @@ const tampilNama = function (nama){
     return `Halo, ${nama}`
 }
 console.log(tampilNama('Mohamad Iqbal Zaelani'))
+
+//disini saya akan menulis uang yang di atas menjadi arrow funtion
+const tampilNama2 = (nama) => {return `Halo, ${nama}`}
+console.log('Iqbal')
+//cara lebih simple menggunakan arrow fucntion
+const tampilNama3 = (nama) => (nama)
+console.log('iqbal')
+
+//bagaimana jika yang di atas lebih dari 1 parameter
+const tampilNama4 = (nama, waktu) => {
+    return `Selamat ${waktu}, ${nama}`
+}
+console.log(tampilNama4('Iqbal', 'Pagi'))
+//cara lebih simple menggunakan arrow function
+const tampilNama5 = (nama, waktu) => (`Selamat ${waktu}, ${nama}` )
+console.log(tampilNama5('Iqbal', 'sore'))
+
+
+//Kasus kedua disini saya mempunyai variable mahasiswa dan saya ingin menghitung masing2 array yang terdapat di mahasiswa
+let mahasiswa = ['Mohamad Iqbal Zaelani', 'Mohamad Sumbul', 'Abdullah Karawitan']
+let jumlahHuruf = mahasiswa.map(function (nama) {
+    return nama.length
+})
+console.log(jumlahHuruf)
+
+//Nah disini saya akan membuat kembali yang di atas tapi berbentuk arrow function
+let mahasiswa2 = ['Iqbal', 'Gama', 'Andika', 'A supri']
+let jumlahHuruf2 = mahasiswa2.map( nama => nama.length)
+console.log(jumlahHuruf2)
+
+//Disini saya akan mencoba mengembalikan sebagai object bukan array dengan menggunakan arrow function
+let mahasiswa3 = ['Iqbal', 'Gama', 'Andika', 'A supri']
+let jumlahHuruf3 = mahasiswa3.map ( nama => ({nama:nama, jlmHuruf3:nama.length}))
+console.log(jumlahHuruf3)
+//disini saya akan mencoba menggunakan console table agar lebih rapih
+console.table(jumlahHuruf3)
