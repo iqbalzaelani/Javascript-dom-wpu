312211b6a9336358f84f8972ecd8c0ee5b9f1463
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-03 09:16:11 +0700

    ArrowFunction

--- Javascript Lanjutan/ArrowFunction.js
+++ Javascript Lanjutan/ArrowFunction.js
@@ -1,26 +1,27 @@
 //Function Declaration
-function tampilPesan(nama){
-    alert('halo' + nama)
-}
-tampilPesan('Iqbal')
+// function tampilPesan(nama){
+//     alert('halo' + nama)
+// }
+// tampilPesan('Iqbal')
 
 //Function Expression
-let tampilPesan2 = function (nama){
-    alert('halo' + nama)
-}
-tampilPesan2('Iqbal')
+// let tampilPesan2 = function (nama){
+//     alert('halo' + nama)
+// }
+// tampilPesan2('Iqbal')
 
 
 //Nah ada beberapa definisi arrow function
 //Bentuk lain yang lebih ringkas dari function expression, jadi kita bisa menggunakan function expression dengan lebih ringkas
 //ini salah satu contohnya
-let tampilPesan3 = (nama) => {
-    alert('halo' + nama)
-}
-tampilPesan3('Iqbal')
+// let tampilPesan3 = (nama) => {
+//     alert('halo' + nama)
+// }
+// tampilPesan3('Iqbal')
 
 //Membuat arrow function
 //saya membuat dulu function expression
 const tampilNama = function (nama){
-    return `Halo, ${this.nama}`
+    return `Halo, ${nama}`
 }
+console.log(tampilNama('Mohamad Iqbal Zaelani'))
