984ddaebc32b25851004d944a4c4f4a43872e1d3
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-11 16:17:41 +0700

    Implementasi Template Literals

--- Javascript Lanjutan/LatihanTemplateLiterals.js
+++ Javascript Lanjutan/LatihanTemplateLiterals.js
@@ -40,7 +40,62 @@ const elmagang = `<div class="magang">
 ${magang.map(m => `<ul>
 <li>${m.nama}</li>
 <li>${m.email}</li>
-</ul>`)}
+</ul>`).join('')}
 </div>`
 //Menampilkan melalui dom body
-document.body.innerHTML = elmagang  
+//di tampilan html ada koma karena dia bentuknya array, cara menghilangkan koma nya dengan chaining (join)
+// document.body.innerHTML = elmagang
+
+
+// 3. Conditionals
+//Pengkondisian
+//ternary
+const lagu = {
+    judul: 'Newcastle',
+    penyanyi: 'Mohamad Iqbal Zaelani',
+    feat: 'Zaelani Iqbal Mohamad'
+}
+//membuat elemen lagi
+const ellagu = `<div class="lagu">
+<ul>
+<li>${lagu.penyanyi}</li>
+<li>${lagu.judul} ${lagu.feat ? `(feat. ${lagu.feat})` : '' }</li>
+</ul>
+</div>`
+//kasusnya ada beberapa lagu yang ada feat/tidak ada feat saya gamau kalo lagu tersebut engga ada feat nya undifiend
+//karena itu disini butuh peng kondisian yaitu yang sebelumnya (feat. ${lagu.feat}) jadi saya bungkus di dalam ${}  
+//jadi jika ada lagu feat tambahkan string tambahkan template literals lagi lalu membuat expresiion lagi yg di dalamnya terdapat lagu dan feat
+//tetapi kalo engga ada kosongin aja  ${lagu.feat ? `(feat. ${lagu.feat})` : ''
+// document.body.innerHTML = ellagu
+
+
+// 4. Nested
+// HTML Fragments bersarang
+const kerja = {
+    nama: 'Website',
+    jumlah: 5,
+    namaBidang: [
+        'FrontEndDeveloper', 
+        'BackEndDeveloper',
+        'Techinal Support', 
+        'UI/UX', 
+        'MobileDeveloper'
+    ]
+}
+//untuk bagian nama bidang saya buat melalui function jadi function terpisah dan memakai expression
+function cetakNamaBidang(namaBidang){
+    return `
+    <ol>
+    ${namaBidang.map(nb => `<li>${nb}</li>`).join('')}
+    </ol>
+    `
+}
+
+//Menampilkan semua data ini ke html fragments
+const elkerja = `<div class="kerja">
+<h2>${kerja.nama}</h2>
+<span class="semester">jumlah bidang : ${kerja.jumlah}</span>
+<h4>Nama Bidang :</h4>
+${cetakNamaBidang(kerja.namaBidang)}
+</div>`
+document.body.innerHTML = elkerja
