f417d5236d3c801858cfc19250d149f902147890
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-11 14:25:34 +0700

    LatihanTemplateLiterals

--- Javascript Lanjutan/LatihanTemplateLiterals.js
+++ Javascript Lanjutan/LatihanTemplateLiterals.js
@@ -14,4 +14,33 @@ const el = `<div class="mhs">
 <p>${mhs.email}</p>
 </div>`
 //Saya mau menampilkan nya ke dom ke body
-document.body.innerHTML = el
+// document.body.innerHTML = el
+
+// 2. Looping
+//Disini menggunakan data yang sudah di gunakan
+//sekarang isinya array yang ada object nya
+const magang = [
+    {
+        nama: 'Zaelani',
+        email: 'zaelani@gmail.com'
+    },
+    {
+        nama: 'Mohamad',
+        email: 'mohamad@gmail.com'
+    },
+    {
+        nama: 'Iqbal',
+        email: 'iqbal@gmail.com'
+    }
+]
+//disini saya mau looping data nya menggunakan data yang ada
+//seperti yang tadi membuat elemen yang nanti di looping nya menggunakan expression method map higher order function ke magang
+//yang di dalamnya terdapat item dan template literals lalu isinya list dan data
+const elmagang = `<div class="magang">
+${magang.map(m => `<ul>
+<li>${m.nama}</li>
+<li>${m.email}</li>
+</ul>`)}
+</div>`
+//Menampilkan melalui dom body
+document.body.innerHTML = elmagang
