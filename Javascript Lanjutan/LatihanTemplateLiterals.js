55ea84ce0a9983d1582ab9ba3a6c8dbebdb4149d
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-11 10:08:48 +0700

    Latihan Template Literals

--- Javascript Lanjutan/LatihanTemplateLiterals.js
+++ Javascript Lanjutan/LatihanTemplateLiterals.js
@@ -6,10 +6,12 @@ const mhs = {
     nrp: '0123321',
     email: 'mohamadiqbaalzaelani@gmail.com'
 }
-//lalu membuat elemennya yang akan di simpan sebagai elemen fragments
+//Lalu membuat elemennya yang akan di simpan sebagai elemen fragments
 const el = `<div class="mhs">
 <h2>${mhs.nama}</h2>
+<p>${mhs.umur}</p>
 <span class="nrp">${mhs.nrp}</span>
+<p>${mhs.email}</p>
 </div>`
-//saya mau menampilkan nya ke dom ke body
+//Saya mau menampilkan nya ke dom ke body
 document.body.innerHTML = el
