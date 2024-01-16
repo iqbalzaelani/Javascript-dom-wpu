5de97561021f54724b366a336409a62f3dddc106
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-16 09:48:43 +0700

    update

--- Javascript Lanjutan/TaggedTemplateLiterals.js
+++ Javascript Lanjutan/TaggedTemplateLiterals.js
@@ -2,14 +2,15 @@
 //Tagged Templates
 const nama = 'Mohamad Iqbal Zaelani'
 const umur = 20
+const email = 'MohamadIqbalZaelani@gmail.com'
 
-function coba(strings, nama, umur){
-    return nama
+function coba(strings, nama, umur, email){
+    return email
 }
 
 // const str = `Halo, nama saya${nama}, saya berumur${umur}` //ini template literals biasa
 
-const str = coba`Halo, nama saya${nama}, saya berumur${umur}` //ini penggunaan tagged template literals
+const str = coba`Halo, nama saya${nama}, saya berumur${umur}, alamat email saya${email}` //ini penggunaan tagged template literals
 //kalo tagged template literals kita akan memasukan tag di dalam function
 //javascript punya 
-console.log(str)
+console.log(str) 
