91c252f7e2147c423e77fa9ce5240bdc48551a63
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-16 16:47:59 +0700

    update

--- Javascript Lanjutan/TaggedTemplateLiterals.js
+++ Javascript Lanjutan/TaggedTemplateLiterals.js
@@ -6,9 +6,24 @@ const email = 'MohamadIqbalZaelani@gmail.com'
 const ntlp = '087820159674'
 const alamat = 'Jl Astanaanyar'
 
-function coba(strings, nama, umur, email, ntlp, alamat){
-    return alamat
+function coba(strings, ...values){
+    let result = ''
+    strings.forEach((str, i) => {
+        result += `${str}${values[i]}`
+    })
+    return result
 }
+//disini saya bikin variable str untuk menampung string nya
+//lalu saya akan looping ke dalam isi values nya
+//jadi str ngambil ke halo nama saya lalu gabung ke index i (iqbal atau expression nya) di looping lagi sampe str selesai
+
+//disini ada race parameter jadi dia bisa menampung seluruh expression yg ada di dalam template literals nya agar tidak cape cape masukin
+//values ini dia akan menjadi array yg isinya semua expression yg ada di dalam template literals nya
+//jadi string untuk ngambil string biasa
+//values buat ngambil expression
+
+//jadi secara default tag template ini bisa menerima parameneter atau argumen contoh  coba(strings, nama, umur, email, ntlp, alamat)
+//jadi ketika di jalankan tag coba ini dia akan memecah tiap string yg ada di dalamnya
 
 // const str = `Halo, nama saya${nama}, saya berumur${umur}` //ini template literals biasa
 
@@ -16,4 +31,4 @@ const str = coba`Halo, nama saya${nama}, saya berumur${umur},
 alamat email saya${email}, no telepon${ntlp}, alamat${alamat}` //ini penggunaan tagged template literals
 //kalo tagged template literals kita akan memasukan tag di dalam function
 //javascript punya  
-console.log(str) 
+console.log(str)    
