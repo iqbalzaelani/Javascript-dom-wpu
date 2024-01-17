4b4041c83dee9120242855d929d8457efc34e034
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-17 11:53:35 +0700

    destructuring

--- Javascript Lanjutan/DestructuringAssignment.js
+++ Javascript Lanjutan/DestructuringAssignment.js
@@ -7,10 +7,10 @@
 
 //ARRAY
 const contoh1 = ['satu', 'dua', 'tiga']
-const [a, b, c] = contoh1
-console.log(a) 
-console.log(b) 
-console.log(c) 
+const [d, e, f] = contoh1
+console.log(d) 
+console.log(e) 
+console.log(f) 
 //jadi misalnya kita punya sebuah variable tetapi isinya array yang di dalamnya juga berupa string
 //nah kita bisa membuat string ini menjadi nilai nilai variable terpisah
 //untuk destructuring assigment ini kita cukup membuat array yang isinya abc kemudian di assign dengan contoh1 sehingga tidak perlu di assign satu2
@@ -39,5 +39,31 @@ const perkenalan = ['Halo', 'nama', 'saya', 'Mohamad Iqbal Zaelani']
 // console.log(nama)
 
 // Kalo pakai Destructuring Array cukup petakan aja yang nantinya di assign ke variable perkenalan yang nanti dia akan langsung jadi variable
-const [halo, nama, saya, mohamadiqbalzaelani] = perkenalan
-console.log(halo)
+const [satu, dua, tiga, empat] = perkenalan
+console.log(empat)
+//kita juga bisa costum kalo yang mau di petakan cuman mau bagian halo sama nama aja
+//skipping items
+//const [satu, , , empat] = perkenalan jadi tinggal tulis aja seperti tadi namun isinya di hapus tapi tanda koma nya jgn di hapus
+
+
+
+// 2.Swap items atau menukar isinya
+let a = 1
+let b = 3
+console.log(a);
+console.log(b);
+//untuk menukar isinya biasanya di butuhkan sebuah variable tetaoi dengan destructuring tinggal
+[a, b] = [b, a]
+console.log(a)
+console.log(b)     //kagak jelas erroy nya 
+//Pelajaran baru kalo mau menulis console.log dengan yg sama parameternya harus di tambahin titik koma(;)
+
+
+// 3.Return value pada function
+//jadi function nya bisa mengembalikan array dan array nya langsung di tangkap oleh sintaks destructurring
+//contoh
+function cobareturn(){
+    return [1, 2]
+}
+const variableA = cobareturn()
+console.log(variableA)
