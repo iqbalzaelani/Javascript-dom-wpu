096b6ae86a174dc3d54f187fa4a7dfa4cbb8931f
iqbalzaelani <mohamadiqbaalzaelani@gmail.com>
2024-01-23 08:43:17 +0700

    update

--- Javascript Lanjutan/Asynchronousjavascript.js
+++ Javascript Lanjutan/Asynchronousjavascript.js
@@ -1,5 +1,29 @@
 // Asynchronous Javascript / Konsep Asynchronous pada javascript
 
+//Javascript 
+//Is a single-threaded, non-blocking, asnychronous and concurrent languange
+
+// Yang maksudnya mulai dari single-treaded
+//  > Thread
+// Urutan eksekusi kode yg dapat dilakukan secara bebas / independent satu sama lain
+// Jadi javascript hanya membaca satu persatu baris perbaris karena itu javascript single
+
+// > Non-Blocking
+// Jadi meskipun task kita belum beres kita bisa lanjut ke task berikutnya
+
+// Kesimpulannya
+// 1. Single vs Multi Thread
+// Lingkungan eksekusi 'task'
+// 2. Blocking vs Non-blocking
+// Teknik 'ngoding' (IO related)
+// 3. Synchronous vs Asynchronous
+// Teknik 'ngoding' (HTTP Request related)
+// 4. Concurrent vs Parallel
+// Lingkungan eksekusi 'task'
+
+//Console itu masuk ke stack
+
+
 // Teknik teknik Asynchronous pada Javascript
 // 1. Callback
 // 2. Promise
